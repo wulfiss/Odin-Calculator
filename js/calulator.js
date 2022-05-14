9cd2a76afc42c60bff7674f0c6aaa09fdcf354a7
Julian <julian_wv@live.com.ar>
2022-05-14 15:43:05 -0300

    add function for subtraction and adding

---
+++ js/calulator.js
@@ -0,0 +1,10 @@
+const suma = (numOne, numTwo) => {
+    return numOne + numTwo;
+};
+
+const resta = (numOne, numTwo) => {
+    return numOne - numTwo;
+};
+
+console.log(suma(1, 2));
+console.log(resta(1, 2));
