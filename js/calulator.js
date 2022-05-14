b09f7523c80a1286e0536c7932875818c287cd6c
Julian <julian_wv@live.com.ar>
2022-05-14 16:17:55 -0300

    Update calulator.js

--- js/calulator.js
+++ js/calulator.js
@@ -8,3 +8,20 @@ const resta = (numOne, numTwo) => {
 
 console.log(suma(1, 2));
 console.log(resta(1, 2));
+
+
+const $one = document.querySelector("#oneB");
+const $two = document.querySelector("#twoB");
+const displayNum = document.querySelector("#displayNum");
+let numOne = "";
+let numTwo = "";
+
+$one.addEventListener('click', e=>{
+    displayNum.textContent += '1';
+    numOne += "1";
+})
+
+$two.addEventListener('click', e=>{
+    displayNum.textContent += '2';
+    numOne += "2";
+})
