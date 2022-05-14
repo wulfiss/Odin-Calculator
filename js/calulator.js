94ab688012576fb6181259125e3aa3adb4ec9240
Julian <julian_wv@live.com.ar>
2022-05-14 18:36:38 -0300

    add subtraction

--- js/calulator.js
+++ js/calulator.js
@@ -1,5 +1,5 @@
 const suma = (numOne, numTwo) => {
-    return numOne + numTwo;
+    return +numOne + +numTwo;
 };
 
 const resta = (numOne, numTwo) => {
@@ -10,18 +10,38 @@ console.log(suma(1, 2));
 console.log(resta(1, 2));
 
 
-const $one = document.querySelector("#oneB");
-const $two = document.querySelector("#twoB");
-const displayNum = document.querySelector("#displayNum");
-let numOne = "";
-let numTwo = "";
+const $one = document.querySelector('#oneB');
+const $two = document.querySelector('#twoB');
+const $add = document.querySelector('#sumaB');
+const $equal = document.querySelector('#equalB');
+const displayNum = document.querySelector('#displayNum');
+let currentValue = '';
+let previousValue = '';
+let operant = '';
 
-$one.addEventListener('click', e=>{
+$one.addEventListener('click', e => {
     displayNum.textContent += '1';
-    numOne += "1";
+    currentValue += "1";
 })
 
-$two.addEventListener('click', e=>{
+$two.addEventListener('click', e => {
     displayNum.textContent += '2';
-    numOne += "2";
+    currentValue += "2";
 })
+
+$add.addEventListener('click', e => {
+    previousValue = currentValue;
+    currentValue = '';
+    displayNum.textContent ='';
+    operant = "suma";
+})
+
+$equal.addEventListener('click', e => {
+    equal(previousValue, currentValue, operant);
+})
+
+const equal = (previousValue, currentValue, operant) => {
+    if (operant == 'suma'){
+        return console.log(suma(previousValue, currentValue));
+    }
+}
